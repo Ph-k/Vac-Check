66caedcf74b6489fe1a770ee992f693591f819db
Philippos Koumparos <filippos_7@hotmail.com>
2022-08-18 00:04:48 +0300

    Moved common

--- Socket-Src/Utilities/HashFunctions.h
+++
@@ -1,2 +0,0 @@
-/*Return the result of the Kth hash funcation*/
-unsigned long hash_i(unsigned char *str, unsigned int i);
--- Socket-Src/Utilities/LogFileWritter.h
+++
@@ -1,5 +0,0 @@
-// Given the process id and a message. It is writen to the log file named log.xxx where xxx the pid
-int writeToLog(char* string);
-
-//Used to print the counties in the logfile while traversing a string dictonary by calling writeToLog
-void dummyCountyLogWritter(void* countryString, void* nothing);
